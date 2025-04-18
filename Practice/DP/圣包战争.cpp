265f476e11b5444139a6a8ee73572576ef17c676
01LuGunag <1754171287@qq.com>
2025-04-18 20:21:32 +0800

    Update 圣包战争.cpp

--- Practice/DP/圣包战争.cpp
+++ Practice/DP/圣包战争.cpp
@@ -35,3 +35,31 @@ int main() {
 
     return 0;
 }
+
+//洛谷：P2871 [USACO07DEC] Charm Bracelet S
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+    vector<int> W(N), V(N);
+    vector<int> dp(M + 1, 0); // dp数组初始化为0
+
+    for(int i = 0; i < N; i ++) {
+        cin >> W[i] >> V[i];
+    }
+
+    for(int i = 0; i < N; i ++) {
+        for(int j = M; j >= W[i]; j --) {
+            dp[j] = max(dp[j], dp[j - W[i]] + V[i]);
+        }
+    }
+
+    cout << dp[M] << endl;
+
+    return 0;
+}
