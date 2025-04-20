58643ebbdd7eae43be4e6e67083a031b14346b00
01LuGunag <1754171287@qq.com>
2025-04-20 23:40:48 +0800

    那个什么日

--- Practice/Coding_Month/那个什么日.cpp
+++ Practice/Coding_Month/那个什么日.cpp
@@ -1,44 +1,43 @@
 #include <iostream>
 #include <iomanip>
+
 using namespace std;
 
-// 判断闰年
-bool is_leap(int year) {
-    return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+bool Leap(int year) {
+    return (year % 4 == 0 && year % 100 != 0 || year % 400 == 0); 
 }
 
-int main() {
+int main () {
     int x;
     cin >> x;
     int year = 1970, month = 1, day = 1;
-    // 每月天数（平年）
-    int month_days[13] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
 
-    day += x; // 加上x天
+    int month_days[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-    while (true) {
-        int days_in_year = is_leap(year) ? 366 : 365;
-        if (day > days_in_year) {
+    day += x;
+
+    while(true) {
+        int days_in_year = 365 + (Leap(year) ? 1 : 0);
+        if(day > days_in_year) {
             day -= days_in_year;
-            year++;
+            year ++;
         } else {
             break;
         }
     }
 
-    while (true) {
-        int days_in_month = month_days[month];
-        if (month == 2 && is_leap(year)) days_in_month = 29;
-        if (day > days_in_month) {
+    while(true) {
+        int days_in_month = month_days[month - 1];
+        if(Leap(year) && month == 2 ) days_in_month = 29;
+        if(day > days_in_month) {
             day -= days_in_month;
-            month++;
+            month ++;
         } else {
             break;
         }
     }
 
-    cout << setw(4) << setfill('0') << year << '-'
-         << setw(2) << setfill('0') << month << '-'
-         << setw(2) << setfill('0') << day << endl;
+    cout << year << "-" << setfill('0') << setw(2) << month << "-" << setfill('0') << setw(2) << day << endl;
+
     return 0;
 }
