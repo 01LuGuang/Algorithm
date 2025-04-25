1ee5d922d2cb6319a0138bfd7d98ebb08816d7e2
01LuGunag <1754171287@qq.com>
2025-04-25 18:36:47 +0800

    Update 麻将.cpp

--- Practice/Coding_Month/麻将.cpp
+++ Practice/Coding_Month/麻将.cpp
@@ -1,21 +1,19 @@
 #include <iostream>
-#include <vector>
+#include <iomanip>
 
 using namespace std;
 
 int main() {
-    int n;
+    int n, x;
     cin >> n;
-    vector<int> a(n);
     double sum = 0.0;
 
     for(int i = 0; i < n; i ++) {
-        cin >> a[i];
-        sum += a[i];
+        cin >> x;
+        sum += x;
     }
 
-    double avg = sum / n;
-
-    printf("%.2f\n", avg);
+    //printf("%.2f\n", sum / n);
+    cout << fixed << setprecision(2) << sum / n << endl;
     return 0;
 }
