b496e2fcf98b81d9e04d3cbf206988b6afc64d37
01LuGunag <1754171287@qq.com>
2025-04-25 14:32:05 +0800

    赌王循环更新

--- Practice/Coding_Month/赌王循环.cpp
+++ Practice/Coding_Month/赌王循环.cpp
@@ -1,3 +1,4 @@
+//思路：直接用两个vector来替换其中长度为 k 的 0 序列
 #include <iostream>
 #include <vector>
 
@@ -6,16 +7,17 @@ using namespace std;
 int main() {
     int n, k;
     cin >> n >> k;
-    vector<int> a(n + 1,0);
+    vector<int> a(n);
     vector<int> b(n + 1,0);
 
-    for(int i = 1; i <= n; i ++) {
+    for(int i = 0; i < n; i ++) {
         cin >> a[i];
     }
 
-    for(int i = k - 1; i <= n; i ++) {
-        int j = 0;
+    int j  = 1;
+    for(int i = k; i < n; i ++) { //注意边界 k 到 n-1
         b[j] = a[i];
+        j ++;
     }
 
     for(int i = 1; i <= n; i ++) {
