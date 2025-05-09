ef74f7cb1633180ec93b7016b34cae47420afe9a
01LuGunag <1754171287@qq.com>
2025-05-09 14:34:56 +0800

    Update 二维网格.cpp

--- Practice/DP/二维网格.cpp
+++ Practice/DP/二维网格.cpp
@@ -14,7 +14,7 @@ int main(){
         return 0;
     }
 
-    vector<vector<int>> dp(N + 1, vector<int>(M + 1, 0));
+    vector<vector<int>> dp(N + 1, vector<int>(M + 1, 0)); //dp[i][j]表示到达(i,j)的最大美丽值
     vector<vector<int>> beauty(N + 1, vector<int>(M + 1, 0));
 
     for(int i = 1; i <= N; i ++) {
@@ -23,8 +23,16 @@ int main(){
         }
     }
 
+    //初始化第一行和第一列
     for(int i = 1; i <= N; i ++) {
-        for(int j = 1; j <= M; j ++) {
+        dp[i][1] = dp[i - 1][1] + beauty[i][1]; //第一列只能从上面来
+    }
+    for(int j = 1; j <= M; j ++) {
+        dp[1][j] = dp[1][j - 1] + beauty[1][j]; //第一行只能从左边来
+    }
+
+    for(int i = 2; i <= N; i ++) {
+        for(int j = 2; j <= M; j ++) {
             dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]) + beauty[i][j];
         }
     }
